61dced5ec4495bebc84cfec4a3b7c8960fe927d3
Lucifer <magicdevstar@gmail.com>
2025-04-16 10:28:17 +1000

    middleware issue fix

--- my-medusa-store/src/api/admin/brands/route.ts
+++ my-medusa-store/src/api/admin/brands/route.ts
@@ -20,11 +20,11 @@ export const POST = async (
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
   const query = req.scope.resolve("query");
   
-  console.log(req.queryConfig)
-  const { data: brands, metadata: { count, take, skip } = {} } = await query.graph({
-    entity: "brand",
-    ...req.queryConfig
-  });
+  const { data: brands, metadata: { count, take, skip } = {} } =
+    await query.graph({
+      entity: "brand",
+      ...req.queryConfig,
+    });
 
-  res.json({ brands });
+  res.json({ brands, count, limit: take, offset: skip });
 };
